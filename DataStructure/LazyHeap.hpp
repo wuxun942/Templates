c3878e56e9366ac531ad57307c51300145dba7f7
wuxun942 <fjsmzhengyubing@outlook.com>
2025-08-01 12:43:38 +0800

    更新 LazyHeap

--- DataStructure/LazyHeap.hpp
+++ DataStructure/LazyHeap.hpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 懒删除堆，需要注意这里的 pop 不同于 STL 中的 pop，是有返回值的。
+
 // 哈希表记录
 template<typename T, typename comp = less<T>>
 class LazyHeap {
@@ -20,6 +22,10 @@ public:
         return sz;
     }
 
+    bool empty() {
+        return sz == 0;
+    }
+
     void remove(T x) {
         ++remove_cnt[x];
         --sz;
@@ -30,11 +36,6 @@ public:
         return pq.top();
     }
 
-    void pop() {
-        apply_remove();
-        pq.pop();
-    }
-
     void push(T x) {
         if (remove_cnt[x] > 0) {
             --remove_cnt[x];
@@ -42,6 +43,26 @@ public:
             pq.push(x);
         }
         ++sz;
+    }
+
+    T pop() {
+        apply_remove();
+        T res = pq.top();
+        pq.pop();
+        --sz;
+        return res;
+    }
+
+    T pushpop(T x) {
+        if (remove_cnt[x] > 0) {
+            --remove_cnt[x];
+        } else {
+            pq.push(x);
+        }
+        apply_remove();
+        T res = pq.top();
+        pq.pop();
+        return res;
     }
 };
 
@@ -51,26 +72,32 @@ class LazyHeap {
     priority_queue<T, vector<T>, comp> pq;
     vector<int> remove_cnt;
     size_t sz = 0;
+    size_t offset;
 
     void apply_remove() {
         while (!pq.empty() && remove_cnt[pq.top()] > 0) {
-            --remove_cnt[pq.top()];
+            --remove_cnt[pq.top() - offset];
             pq.pop();
         }
     }
 
 public:
-    // 传入堆中数据的最大值
-    LazyHeap(int mx) {
-        remove_cnt.resize(mx + 1);
+    // 传入堆中数据的最大值和最小值
+    LazyHeap(int mx, int mn) {
+        remove_cnt.resize(mx - mn + 1);
+        offset = mn;
     }
 
     size_t size() {
         return sz;
     }
 
+    bool empty() {
+        return sz == 0;
+    }
+
     void remove(T x) {
-        ++remove_cnt[x];
+        ++remove_cnt[x - offset];
         --sz;
     }
 
@@ -79,17 +106,32 @@ public:
         return pq.top();
     }
 
-    void pop() {
-        apply_remove();
-        pq.pop();
-    }
-
     void push(T x) {
-        if (remove_cnt[x] > 0) {
-            --remove_cnt[x];
+        if (remove_cnt[x - offset] > 0) {
+            --remove_cnt[x - offset];
         } else {
             pq.push(x);
         }
         ++sz;
+    }
+
+    T pop() {
+        apply_remove();
+        T res = pq.top();
+        pq.pop();
+        --sz;
+        return res;
+    }
+
+    T pushpop(T x) {
+        if (remove_cnt[x - offset] > 0) {
+            --remove_cnt[x - offset];
+        } else {
+            pq.push(x);
+        }
+        apply_remove();
+        T res = pq.top();
+        pq.pop();
+        return res;
     }
 };

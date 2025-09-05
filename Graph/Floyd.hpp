b7ef078b9ef2884074643e39643d45652ec32418
wuxun942 <fjsmzhengyubing@outlook.com>
2025-09-05 15:06:43 +0800

    小改

--- Graph/Floyd.hpp
+++ Graph/Floyd.hpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // Floyd 算法
 vector<vector<long long>> floyd(int n, const vector<vector<int>>& edges) {
-    vector f(n, vector(n, LLONG_MAX));
+    vector f(n, vector(n, LLONG_MAX / 2));
     for (auto& e: edges) {
         int i = e[0], j = e[1], w = e[2];
         f[i][j] = w;
@@ -11,11 +11,11 @@ vector<vector<long long>> floyd(int n, const vector<vector<int>>& edges) {
     }
     for (int k = 0; k < n; ++k) {
         for (int i = 0; i < n; ++i) {
-            if (f[i][k] == LLONG_MAX) {
+            if (f[i][k] == LLONG_MAX / 2) {
                 continue;
             }
             for (int j = 0; j < n; ++j) {
-                f[i][j] = min(f[i][j], 0LL + f[i][k] + f[k][j]);
+                f[i][j] = min(f[i][j], f[i][k] + f[k][j]);
             }
         }
     }

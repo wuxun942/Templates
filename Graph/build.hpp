31eb473a15d288992f7f296c7f8e135781d5ed0f
wuxun942 <fjsmzhengyubing@outlook.com>
2025-09-23 15:11:57 +0800

    添加链式前向星

---
+++ Graph/build.hpp
@@ -0,0 +1,15 @@
+// 链式前向星建图，以下为 1-index
+
+constexpr int MAXN = 5001, MAXM = 500'001;
+int head[MAXN]{};
+int nxt[MAXM];
+int to[MAXM];
+// int weight[MAXM];
+int cnt_e = 0;
+void add_e(int x, int y, int w = 0) {
+    ++cnt_e;
+    nxt[cnt_e] = head[x];
+    to[cnt_e] = y;
+    // weight[cnt_e] = w;
+    head[x] = cnt_e;
+}

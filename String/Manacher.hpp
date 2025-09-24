45aea2249a01de63e4bf32003e654aa28a8137dc
wuxun942 <fjsmzhengyubing@outlook.com>
2025-09-24 13:31:34 +0800

    微调

--- String/Manacher.hpp
+++ String/Manacher.hpp
@@ -3,20 +3,19 @@ using namespace std;
 
 // Manacher: 查找以每个位置为中心的最长回文子串
 vector<int> manacher(const string& s) {
-    int m = s.size();
+    int n = s.size();
     // s[i] = t[2 * i + 2]
-    string t(2 * m + 3, '#');
+    string t(2 * n + 3, '#');
     t[0] = '^';
-    t[2 * m + 2] = '$';
-    for (int i = 0; i < m; ++i) {
+    t[2 * n + 2] = '$';
+    for (int i = 0; i < n; ++i) {
         t[2 * i + 2] = s[i];
     }
-    int n = t.size();
-    vector half_len(n - 2, 0);
-    half_len[1] = 1;
+    int m = t.size();
+    vector half_len(m - 2, 0);
     vector<pair<int, int>> ans;
     // (2 * box_m - box_r, box_r) 是镜像的
-    for (int i = 2, box_m = 0, box_r = 0; i < n - 2; ++i) {
+    for (int i = 1, box_m = 0, box_r = 0; i < m - 1; ++i) {
         // hl: t[i] 的最大回文半径，实际回文串长度等于 hl - 1
         int hl = 1;
         if (i < box_r) {

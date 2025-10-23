487132c76005d0b4b11c0a1aaa41a9811dbe74be
DBaker <you@example.com>
2025-10-23 16:07:22 +0800

    调整组合数预处理顺序

--- Math/Combinatorics.hpp
+++ Math/Combinatorics.hpp
@@ -1,9 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 预处理阶乘 / 组合数：注意区分带模和不带模
+// 预处理阶乘 / 组合数
 
-// 阶乘逆元 + 带模组合数
+// 1. 不预处理，直接计算组合数
+long long comb(int n, int m) {
+    if (n < m) {
+        return 0;
+    }
+    m = min(m, n - m);
+    long long res = 1;
+    for (int i = 1; i <= m; i++) {
+        res = res * (n + 1 - i) / i; // 注意计算顺序
+    }
+    return res;
+}
+
+// 2. 递推预处理组合数
+constexpr int MAXN = 101;
+long long c[MAXN][MAXN];
+auto init = []() {
+    memset(c, 0, sizeof(c));
+    c[0][0] = 1;
+    for (int i = 1; i < MAXN; ++i) {
+        c[i][0] = c[i][i] = 1;
+        for (int j = 1; j < i; ++j) {
+            c[i][j] = c[i - 1][j - 1] + c[i - 1][j];
+        }
+    }
+    return 0;
+}();
+
+// 3. 质数模下的预处理：阶乘逆元 + 带模组合数
 constexpr int MOD = 1'000'000'007;
 long long qpow(long long x, long long n, int mod) {
     int ans = 1;
@@ -16,15 +44,15 @@ long long qpow(long long x, long long n, int mod) {
     return ans;
 }
 
-constexpr int MX = 200'001;
-long long fac[MX], inv_f[MX];
+constexpr int MAXN = 200'001;
+long long fac[MAXN], inv_f[MAXN];
 auto init = []() {
     fac[0] = 1;
-    for (int i = 1; i < MX; i++) {
+    for (int i = 1; i < MAXN; i++) {
         fac[i] = fac[i - 1] * i % MOD;
     }
-    inv_f[MX - 1] = qpow(fac[MX - 1], MOD - 2, MOD);
-    for (int i = MX - 2; i >= 0; i--) {
+    inv_f[MAXN - 1] = qpow(fac[MAXN - 1], MOD - 2, MOD);
+    for (int i = MAXN - 2; i >= 0; i--) {
         inv_f[i] = inv_f[i + 1] * (i + 1) % MOD;
     }
     return 0;
@@ -34,40 +62,21 @@ int comb(int n, int m, int mod) {
     return n < m ? 0 : fac[n] * inv_f[m] % mod * inv_f[n - m] % mod;
 }
 
-// 不带模组合数
-
-// 不预处理
-long long comb(int n, int m) {
-    if (n < m) {
-        return 0;
-    }
-    m = min(m, n - m);
-    long long res = 1;
-    for (int i = 1; i <= m; i++) {
-        res = res * (n + 1 - i) / i;
-    }
-    return res;
-}
-
-// 预处理
-constexpr int MX = 101;
-long long c[MX][MX];
-auto init = []() {
-    memset(c, 0, sizeof(c));
-    c[0][0] = 1;
-    for (int i = 1; i < MX; ++i) {
-        c[i][0] = c[i][i] = 1;
-        for (int j = 1; j < i; ++j) {
-            c[i][j] = c[i - 1][j - 1] + c[i - 1][j];
-        }
-    }
-    return 0;
-}();
-
+// 4. 小质数模下的更大范围计算
 // Lucas 定理：mod 必须是质数
 int Lucas(int n, int m, int mod) {
     if (n < mod && m < mod) {
         return c[n][m];
     }
-    return 1LL * c[n % mod][m % mod] * Lucas(n / mod, m / mod, mod);
+    return 1LL * c[n % mod][m % mod] * Lucas(n / mod, m / mod, mod) % mod;
 }
+
+/*
+扩展 Lucas：推广到 mod 为一般数的情况
+
+把 mod 质因数分解为 p1 ** q1 * p2 ** q2 * ...
+令 mi = pi ** qi
+由 Legendre 公式先求出 ri = x % mi
+由扩展 CRT，x 的通解为 mod * y + x0
+那么 x0 才是最后取模的结果
+*/

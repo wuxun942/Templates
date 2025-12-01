2574ddae21d339bf04583bddcae1f48fae49b93d
DBaker <you@example.com>
2025-12-01 16:30:31 +0800

    Splay树添加注释

--- DataStructure/Splay.hpp
+++ DataStructure/Splay.hpp
@@ -1,8 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+using T = long long;
+constexpr T INF = LLONG_MAX;
+
 // 最大节点数量
-constexpr int MAXN = 100001;
+constexpr int MAXN = 100'001;
 
 // 空间使用计数
 int cnt = 0;
@@ -11,7 +14,7 @@ int cnt = 0;
 int head = 0;
 
 // 节点的 key
-int key[MAXN];
+T key[MAXN];
 
 // 节点的父节点
 int fa[MAXN];
@@ -38,25 +41,29 @@ void up(int i) {
     siz[i] = siz[ls[i]] + siz[rs[i]] + 1;
 }
 
+// 判断节点是左儿子还是右儿子
+// 规定：头节点是左儿子
 int lr(int i) {
     return rs[fa[i]] == i ? 1 : 0;
 }
 
+// 通过旋转让 i 上升一步
 void rotate(int i) {
     int f = fa[i], g = fa[f], soni = lr(i), sonf = lr(f);
-    if (soni == 1) {
-        rs[f] = ls[i];
+    if (soni == 1) { // i 是右儿子，得左旋
+        rs[f] = ls[i]; // f 的右儿子设为 i 的左儿子
         if (rs[f] != 0) {
             fa[rs[f]] = f;
         }
         ls[i] = f;
-    } else {
-        ls[f] = rs[i];
+    } else { // i 是左儿子，得右旋
+        ls[f] = rs[i]; // f 的左儿子设为 i 的右儿子
         if (ls[f] != 0) {
             fa[ls[f]] = f;
         }
         rs[i] = f;
     }
+    // 如果存在爷爷节点 g，需要额外设置 g 和 i 的关系
     if (g != 0) {
         if (sonf == 1) {
             rs[g] = i;
@@ -70,13 +77,16 @@ void rotate(int i) {
     up(i);
 }
 
+// 提根操作：使 i 成为 goal 的儿子
+// goal = 0 时，i 直接成为头节点
 void splay(int i, int goal) {
     int f = fa[i], g = fa[f];
     while (f != goal) {
+        // 爷爷不是目标节点，至少要上升两步
         if (g != goal) {
-            if (lr(i) == lr(f)) {
+            if (lr(i) == lr(f)) { // 一字型 zig-zig，先调整 f，再调整 i
                 rotate(f);
-            } else {
+            } else { // 之字形 zig-zag，i 上升两步
                 rotate(i);
             }
         }
@@ -89,15 +99,16 @@ void splay(int i, int goal) {
     }
 }
 
-int find(int rank) {
+// 查找第 k 小节点的编号，如果不存在则返回 0
+int find(int k) {
     int i = head;
     while (i != 0) {
-        if (siz[ls[i]] + 1 == rank) {
+        if (siz[ls[i]] + 1 == k) {
             return i;
-        } else if (siz[ls[i]] >= rank) {
+        } else if (siz[ls[i]] >= k) {
             i = ls[i];
         } else {
-            rank -= siz[ls[i]] + 1;
+            k -= siz[ls[i]] + 1;
             i = rs[i];
         }
     }
@@ -105,13 +116,18 @@ int find(int rank) {
 }
 
 // 插入节点
-void insert(int x) {
+void insert(T x) {
     key[++cnt] = x;
     siz[cnt] = 1;
-    if (head == 0) {
+    if (head == 0) { // 空树
         head = cnt;
     } else {
-        int f = 0, i = head, son = 0;
+        // 当前节点 i
+        int i = head;
+        // i 的父亲
+        int f = 0;
+        // i 是 f 的哪个儿子
+        int son = 0;
         while (i != 0) {
             f = i;
             if (key[i] <= x) {
@@ -128,12 +144,13 @@ void insert(int x) {
             ls[f] = cnt;
         }
         fa[cnt] = f;
+        // 插入后提根
         splay(cnt, 0);
     }
 }
 
 // 查找排名
-int get_rank(int x) {
+int get_rank(T x) {
     int i = head, last = head;
     int ans = 0;
     while (i != 0) {
@@ -150,16 +167,16 @@ int get_rank(int x) {
 }
 
 // 查询第 k 小的数字
-int index(int k) {
+T index(int k) {
     int i = find(k);
     splay(i, 0);
     return key[i];
 }
 
 // 查找 x 的前驱
-int pre(int x) {
+T pre(T x) {
     int i = head, last = head;
-    int ans = INT_MIN;
+    T ans = -INF;
     while (i != 0) {
         last = i;
         if (key[i] >= x) {
@@ -174,9 +191,9 @@ int pre(int x) {
 }
 
 // 查找 x 的后继
-int post(int x) {
+T post(T x) {
     int i = head, last = head;
-    int ans = INT_MAX;
+    T ans = INF;
     while (i != 0) {
         last = i;
         if (key[i] <= x) {
@@ -191,21 +208,28 @@ int post(int x) {
 }
 
 // 删除节点
-void remove(int x) {
-    int kth = get_rank(x);
-    if (kth != get_rank(x + 1)) {
-        int i = find(kth);
+void remove(T x) {
+    int k = get_rank(x);
+    // 存在 key = x 的节点
+    if (k != get_rank(x + 1)) {
+        // 获得节点编号
+        int i = find(k);
+        // 先提根
         splay(i, 0);
         if (ls[i] == 0) {
             head = rs[i];
         } else if (rs[i] == 0) {
             head = ls[i];
-        } else {
-            int j = find(kth + 1);
+        } else { // 既有左儿子又有右儿子
+            // 查找第 k + 1 小的节点
+            int j = find(k + 1);
+            // 把 j 变成 i 的儿子，那么 j 一定没有左儿子
             splay(j, i);
+            // i 的左儿子变成 j 的左儿子
             ls[j] = ls[i];
             fa[ls[j]] = j;
             up(j);
+            // 头节点变为 j，i 不再被访问（相当于删除）
             head = j;
         }
         if (head != 0) {

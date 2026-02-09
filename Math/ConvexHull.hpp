65a0a58641df7b932a0c261c7b782ec9fb25133c
wuxun942 <fjsmzhengyubing@outlook.com>
2026-02-09 22:19:30 +0800

    调整向量乘积运算

--- Math/ConvexHull.hpp
+++ Math/ConvexHull.hpp
@@ -3,13 +3,18 @@ using namespace std;
 
 // Andrew 算法求凸包（删除所有共线点）
 template<typename T>
-T dot(T x1, T y1, T x2, T y2, T x3, T y3) {
-    return (x2 - x1) * (x3 - x2) + (y2 - y1) * (y3 - y2);
+array<T, 2> sub(array<T, 2> v1, array<T, 2> v2) {
+    return {v1[0] - v2[0], v1[1] - v2[1]};
 }
 
-template<typename T>
-T det(T x1, T y1, T x2, T y2, T x3, T y3) {
-    return (x2 - x1) * (y3 - y2) - (y2 - y1) * (x3 - x2);
+template<typename T, typename F = T>
+F dot(array<T, 2> v1, array<T, 2> v2) {
+    return (F) v1[0] * v2[0] + (F) v1[1] * v2[1];
+}
+
+template<typename T, typename F = T>
+F det(array<T, 2> v1, array<T, 2> v2) {
+    return (F) v1[0] * v2[1] - (F) v1[1] * v2[0];
 }
 
 vector<array<int, 2>> andrew(vector<vector<int>> &points) {
@@ -18,14 +23,14 @@ vector<array<int, 2>> andrew(vector<vector<int>> &points) {
     st.push_back({points[0][0], points[0][1]});
     for (int i = 1; i < n; ++i) {
         int x = points[i][0], y = points[i][1];
-        while (st.size() > 1 && det<int>(st[st.size() - 2][0], st[st.size() - 2][1], st.back()[0], st.back()[1], x, y) <= 0) {
+        while (st.size() > 1 && det<int>(sub(st.back(), st[st.size() - 2]), sub({x, y}, st.back())) <= 0) {
             st.pop_back();
         }
         st.push_back({x, y});
     }
     for (int i = n - 2, m = st.size(); i >= 0; --i) {
         int x = points[i][0], y = points[i][1];
-        while (st.size() > m && det<int>(st[st.size() - 2][0], st[st.size() - 2][1], st.back()[0], st.back()[1], x, y) <= 0) {
+        while (st.size() > m && det<int>(sub(st.back(), st[st.size() - 2]), sub({x, y}, st.back())) <= 0) {
             st.pop_back();
         }
         st.push_back({x, y});
